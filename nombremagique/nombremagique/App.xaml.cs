91ca0ee32b4d471f81d3c2be7e7ecbce59ab7d48
william fundaro <wfundaro@gmail.com>
2020-10-05 19:43:57 +0200

    init project

---
+++ nombremagique/nombremagique/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using nombremagique.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace nombremagique
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new GamePage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ nombremagique/nombremagique/Views/GamePage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace nombremagique.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GamePage : ContentPage
+    {
+        const int NB_MAGIQUE_MIN = 1;
+        const int NB_MAGIQUE_MAX = 10;
+        int nombreMagique = 0;
+        public GamePage()
+        {
+            InitializeComponent();
+            nombreMagique = new Random().Next(NB_MAGIQUE_MIN, NB_MAGIQUE_MAX);
+            minMaxLabel.Text = "entre " + NB_MAGIQUE_MIN + " et " + NB_MAGIQUE_MAX;
+        }
+
+        public void ButtonClick(object sender, System.EventArgs args)
+        {
+            if (String.IsNullOrWhiteSpace(numberEntry.Text))
+            {
+                DisplayAlert("Oops", "Vous devez entrer un nombre", "ok");
+                return;
+            }
+            int nombreUtilisateur = 0;
+            try
+            {
+                nombreUtilisateur = Int32.Parse(numberEntry.Text);
+            }
+            catch (Exception e)
+            {
+                DisplayAlert("Oops", "Vous devez entreruniquement des chiffres", "ok");
+                return;
+            }
+            if ((nombreUtilisateur < NB_MAGIQUE_MIN) || (nombreUtilisateur > NB_MAGIQUE_MAX))
+            {
+                DisplayAlert("Oops", "Vous devez entrer un nombre entre " + NB_MAGIQUE_MIN + " et " + NB_MAGIQUE_MAX, "ok");
+                return;
+            }
+            if (nombreUtilisateur < nombreMagique)
+            {
+                DisplayAlert("Oops", "Le nombre magique est supérieur à " + nombreUtilisateur, "ok");
+                return;
+            }
+            if (nombreUtilisateur > nombreMagique)
+            {
+                DisplayAlert("Oops", "Le nombre magique est inférieur à " + nombreUtilisateur, "ok");
+                return;
+            }
+            if (nombreUtilisateur == nombreMagique)
+            {
+                DisplayAlert("Gagné", "Vous avez trouvé le nombre magique", "ok");
+            }
+        }
+    }
+}
