b378f8903f147bed9c457547d6bab0294386fd5b
william fundaro <wfundaro@gmail.com>
2020-10-06 14:36:17 +0200

    add stars view

---
+++ nombremagique/nombremagique/Views/StarsView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace nombremagique.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StarsView : ContentView
+    {
+        public StarsView()
+        {
+            InitializeComponent();
+#pragma warning disable CS4014
+            InfiniteRotation(star1, 5000);
+            InfiniteRotation(star2, 7000);
+            InfiniteRotation(star3, 9000);
+#pragma warning restore CS4014
+        }
+        private async Task InfiniteRotation(View view, uint length)
+        {
+            while (true)
+            {
+                await view.RotateTo(360, length);
+                view.Rotation = 0;
+            }
+        }
+    }
+}
---
+++ nombremagique/nombremagique/Views/WinPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace nombremagique.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WinPage : ContentPage
+    {
+        public WinPage() : this(5)
+        {
+
+        }
+        public WinPage(int nombreMagique)
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+            mainLayout.Scale = 0.7;
+            mainLayout.ScaleTo(1.0, 1500, Easing.BounceIn);
+            labelMagicNumber.Text = "Le nombre magique était : " + nombreMagique;
+            NavigateBackToWelcomePage();
+        }
+        private async Task NavigateBackToWelcomePage()
+        {
+            await Task.Delay(3000);
+            await Navigation.PopToRootAsync();
+        }
+    }
+}
